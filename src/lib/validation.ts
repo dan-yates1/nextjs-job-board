3a1143953f453abadbc1c15bd97b31ead0cd8eea
Daniel Yates <dsyates@live.co.uk>
2024-07-28 20:55:19 +0100

    created zod schema for job submissions

--- src/lib/validation.ts
+++ src/lib/validation.ts
@@ -1,4 +1,71 @@
 import { z } from "zod";
+import { jobTypes, locationTypes } from "./job-types";
+
+const requiredString = z.string().min(1, "Required");
+const numericRequiredString = requiredString.regex(/^\d+$/, "Must be a number");
+
+const companyLogoSchema = z
+  .custom<File | undefined>()
+  .refine(
+    (file) => !file || (file instanceof File && file.type.startsWith("image/")),
+    "Must be an image file"
+  )
+  .refine((file) => {
+    return !file || file.size < 1024 * 1024 * 2;
+  }, "File must be less than 2MB");
+
+const applicationSchema = z
+  .object({
+    applicationEmail: z.string().max(100).email().optional().or(z.literal("")),
+    applicationUrl: z.string().max(100).url().optional().or(z.literal("")),
+  })
+  .refine((data) => data.applicationEmail || data.applicationUrl, {
+    message: "Must provide either an email or a URL",
+    path: ["applicationEmail"],
+  });
+
+const locationSchema = z
+  .object({
+    locationType: requiredString.refine(
+      (value) => locationTypes.includes(value),
+      "Invalid location type"
+    ),
+    location: z.string().max(100).optional(),
+  })
+  .refine(
+    (data) =>
+      !data.locationType || data.locationType === "remote" || data.location,
+    {
+      message: "Must provide a location for on-site jobs",
+      path: ["location"],
+    }
+  );
+
+export const createJobSchema = z
+  .object({
+    title: requiredString.max(100, "Title must be less than 100 characters"),
+    type: requiredString.refine(
+      (value) => jobTypes.includes(value),
+      "Invalid job type"
+    ),
+    companyName: requiredString.max(
+      100,
+      "Company name must be less than 100 characters"
+    ),
+    companyLogo: companyLogoSchema,
+    description: z
+      .string()
+      .max(5000, "Description must be less than 5000 characters")
+      .optional(),
+    salary: numericRequiredString.max(
+      9,
+      "Number can't be longer than 9 digits"
+    ),
+  })
+  .and(applicationSchema)
+  .and(locationSchema);
+
+  export type CreateJobValues = z.infer<typeof createJobSchema>;
 
 export const jobFilterSchema = z.object({
   q: z.string().optional(),
